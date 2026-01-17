b02ef2ee1ee1f5e78a9dbf2197405eac6911fb2e
styleoflife987-hub <styleoflife987@gmail.com>
2026-01-17 22:32:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,9 +13,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 def main():
     app = Application.builder().token(TOKEN).build()
-
     app.add_handler(CommandHandler("start", start))
-
     print("🤖 Bot started...")
     app.run_polling()
 
